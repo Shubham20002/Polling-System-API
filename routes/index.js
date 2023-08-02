06b4e8fc8c05166e237e9d07379deb3b9de2a758
shubham <shubham992284@gmail.com>
2023-08-02 11:49:05 +0530

    home route added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express=require('express');
-
+//using express router
 const router=express.Router();
+const questioncontroller=require('../controllers/questioncontroller');
 
-
-
+router.get('/',questioncontroller.questions);
 router.use('/questions',require('./question'));
 router.use('/options',require('./option'));
 
